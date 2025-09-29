2918315f5bc91dc0a4eabc71b53231b053acbaaa
manoj <manoj@medprimetech.com>
2025-09-29 17:23:18 +0530

    Initial Commit

---
+++ NoteApp/NoteAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoteAppApp.swift
+//  NoteApp
+//
+//  Created by Manoj on 29/09/25.
+//
+
+import SwiftUI
+
+@main
+struct NoteAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
